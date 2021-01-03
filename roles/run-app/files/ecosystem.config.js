3a455cb86051c8bc0b512098d3612fbb356abe69
CSamuelLents <sam@samlents.com>
2021-01-03 14:26:03 -0500

    project files - initial commit

--- roles/run-app/files/ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apps: [
-    {
-      name: 'express-server',
-      script: 'index.js',
-    },
-  ],
-};
--- roles/run-app/files/index.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-var server = http.createServer((_req, res) => {
-  res.writeHead(200);
-  res.end('Hello world!');
-});
-
-server.listen(3000);
