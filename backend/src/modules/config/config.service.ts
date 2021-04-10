68cf55266964b50060d3470323e4b4e9ee795dfd
CSamuelLents <sam@samlents.com>
2021-04-10 18:12:49 -0400

    debugging

--- backend/src/modules/config/config.service.ts
+++ backend/src/modules/config/config.service.ts
@@ -55,8 +55,9 @@ export class ConfigService {
       // AUTH0_DOMAIN: Joi.string().required(),
       // AUTH0_AUDIENCE: Joi.string().required(),
     }).unknown();
-    const { error, value: validatedEnvConfig } = envVarsSchema.validate(envConfig);
-
+    const { error, value: validatedEnvConfig } = envVarsSchema.validate(
+      envConfig,
+    );
 
     if (error) {
       throw new Error(`Config validation error: ${error.message}`);
